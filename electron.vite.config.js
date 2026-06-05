59f9c06331fcf2379d5ac02232940c117dc2e376
Dark Waves Tech <89140466+darkwaves-ofc@users.noreply.github.com>
2026-06-05 18:28:42 +0530

    update electrone!

--- electron.vite.config.js
+++ electron.vite.config.js
@@ -31,6 +31,9 @@ export default defineConfig({
     }
   },
   renderer: {
+    css: {
+      postcss: false
+    },
     resolve: {
       alias: {
         // '@renderer': resolve('src/renderer/src'),
